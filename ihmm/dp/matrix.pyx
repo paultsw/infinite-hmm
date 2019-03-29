66cb74c1d477faedd085caa903d96b2f3eb1ff25
Paul Tang <paulswtang@gmail.com>
2019-03-29 15:55:59 -0400

    checkpoint

--- ihmm/dp/matrix.pyx
+++ ihmm/dp/matrix.pyx
@@ -2,48 +2,45 @@
 """
 Cython-based implementation of an expanding matrix table (i.e., the number
 of rows and columns in the matrix are not fixed).
-
-Makes use of the std::map class from the C++ STL.
 """
 # numerics:
-import numpy as npy
-cimport numpy as npc
-from libcpp.map cimport cmap
-from libcpp.pair cimport pair
+import numpy as np
+cimport numpy as cnp
 
-cdef cppclass MatrixTable:
-    cdef __cinit__(self):
+cdef cppclass Matrix:
+    cdef __cinit__(self, limit):
+        # [-[-[ TODO ]-]-]
         # _data hold the raw counts:
-        cdef cmap[pair[int, int], int] self._data
-        # _entry is a placeholder tuple for (row,col):
-        cdef pair[int, int] self._entry
-        # _total contains the running total number of observations:
+        cdef vector[int] _data = new vector
+        # _total contains the running total number of observations;
+        # _nrows and _ncols are the number of rows and columns, respectively.
         cdef unsigned int self._total=0, self._nrows=0, self._ncols=0
         if self._data == NULL:
             raise MemoryError("[MatrixTable] could not allocate memory.")
 
-    def __dealloc__(self):
+    cdef __dealloc__(self):
         """C-level teardown: delete data table."""
         if self._data != NULL:
             del self._data
 
     def __init__(self, **kwargs):
         """Python-level initialization."""
+        # [-[-[ TODO ]-]-]
         pass
 
     def __getitem__(self, pair):
         """Getter-overloading for the bracket-indexing operators `[,]`."""
+        # [-[-[ TODO ]-]-]
         row, col = pair
-        # TODO
 
     def __setitem__(self, pair, val):
         """Setter-overloading for the bracket-indexing operators `[,]`."""
         # check dimensions of pair, val:
-        pass # TODO
+        pass # [-[-[ TODO ]-]-]
 
     def to_numpy(self):
         """Return a c-level numpy array."""
-        pass # TODO
+        pass # [-[-[ TODO ]-]-]
 
     def shape(self):
         """Return current shape of the underlying matrix."""
